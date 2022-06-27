a5bf5f3588cc88a9b1f78bf72426e90311efa26b
Jędrzej Wojcieszak <jedwojc@gmail.com>
2022-06-28 01:06:27 +0200

    html improvements

--- src/app/basketHandler.test.js
+++ src/app/basketHandler.test.js
@@ -2,19 +2,25 @@
  * @jest-environment jsdom
  */
 
-import {Basket} from "./basketHandler";
+import {
+    Basket
+} from "./basketHandler";
 
 describe("Basket", () => {
     const basket = new Basket();
-  
-    test("defines setRule()", () => {
-      expect(typeof basket.totalPrice).toBe("function");
+
+    test("defines totalPrice()", () => {
+        expect(typeof basket.totalPrice).toBe("function");
     });
 
     test("totalPrice should output 3.00", () => {
-        const totalPriceSpy = jest.spyOn(basket,"totalPrice");
-        const result = basket.totalPrice([{price:1},{price:2}]);
+        const totalPriceSpy = jest.spyOn(basket, "totalPrice");
+        const result = basket.totalPrice([{
+            price: 1
+        }, {
+            price: 2
+        }]);
         expect(result).toBe('3.00');
         totalPriceSpy.mockClear();
     });
-  });
+});
